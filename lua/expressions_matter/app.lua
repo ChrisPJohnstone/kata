508b6dd8d7ad997a15dd58b617aa40a070c9b7eb
Christopher Johnstone <chrispjohnstone97@gmail.com>
2024-07-30 20:26:32 +0100

    Switched lua to 2 space tabs as per style guide

--- lua/expressions_matter/app.lua
+++ lua/expressions_matter/app.lua
@@ -1,7 +1,7 @@
 return function(a, b, c)
-    plus_plus = a + b + c
-    plus_mult = (a + b) * c
-    mult_plus = a * (b + c)
-    mult_mult = a * b * c
-    return math.max(plus_plus, plus_mult, mult_plus, mult_mult)
+  plus_plus = a + b + c
+  plus_mult = (a + b) * c
+  mult_plus = a * (b + c)
+  mult_mult = a * b * c
+  return math.max(plus_plus, plus_mult, mult_plus, mult_mult)
 end
